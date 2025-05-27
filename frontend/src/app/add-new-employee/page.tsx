551e8d3336b60627315913332989bff9c3e3af45
mhankazis <mhankazis@gmail.com>
2025-05-27 16:17:50 +0700

    feat:fix-sidebar-add-new-employee

--- frontend/src/app/add-new-employee/page.tsx
+++ frontend/src/app/add-new-employee/page.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import {
   Calendar as CalendarIcon,
-  User,
+  Grid,
   Users,
   Clock,
   MessageCircle,
@@ -42,15 +43,29 @@ export default function AddNewEmployeePage() {
       <aside className="w-16 flex flex-col justify-between items-center bg-gray-100 py-4">
         <div className="flex flex-col items-center gap-6">
           <Image src="/HRIS.png" alt="Logo" width={32} height={32} />
-          <User className="w-5 h-5 text-gray-600" />
-          <Users className="w-5 h-5 text-gray-600" />
-          <Clock className="w-5 h-5 text-gray-600" />
-          <CalendarIcon className="w-5 h-5 text-gray-600" />
-          <MessageCircle className="w-5 h-5 text-gray-600" />
+          <Link href="/dashboard">
+            <Grid className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
+          <Link href="/employee-database">
+            <Users className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
+          <Link href="/checkclock">
+            <Clock className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
+          <Link href="/pricing-package">
+            <CalendarIcon className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
+          <Link href="/order-summary">
+            <MessageCircle className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
         </div>
         <div className="flex flex-col items-center gap-4 mb-4">
-          <Headphones className="w-5 h-5 text-gray-600" />
-          <Settings className="w-5 h-5 text-gray-600" />
+          <Link href="/headphones">
+            <Headphones className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
+          <Link href="/settings">
+            <Settings className="w-5 h-5 text-gray-600 cursor-pointer" />
+          </Link>
         </div>
       </aside>
 
