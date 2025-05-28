118af742ad79a9460ab1efaeff216b0f05b12987
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2025-05-28 21:19:48 +0700

    phone

--- backend/laravel/database/migrations/2025_05_26_103721_add_phone_to_users_table.php
+++ backend/laravel/database/migrations/2025_05_26_103721_add_phone_to_users_table.php
@@ -1,28 +1,28 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->unique()->nullable()->after('email');
-        });
-    }
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::table('users', function (Blueprint $table) {
+                // $table->string('phone')->unique()->nullable()->after('email');
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('phone');
-        });
-    }
-};
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('phone');
+            });
+        }
+    };
