4e9caf772d2631f56b6b2b6e6838445481437d90
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2025-05-29 12:51:47 +0700

    next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import path from 'path';
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
