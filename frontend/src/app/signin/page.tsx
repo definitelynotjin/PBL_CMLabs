d4cf7deeac6258820399177aaccc98e30eefa588
definitelynotjin <118191197+definitelynotjin@users.noreply.github.com>
2025-05-29 20:58:41 +0700

    wat

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -71,7 +71,7 @@ export default function SignInPage() {
               <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
                 <GalleryVerticalEnd className="size-4" />
               </div>
-              Acme Inc.
+              Ame Inc.
             </a>
           </div>
           <div className="flex flex-1 items-center justify-center">
