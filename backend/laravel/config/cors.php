7dec7cd0205da230e28ee7b1508d90956fd93f38
ssh definitelynotjin <gkestiyanto@gmail.com>
2025-06-03 09:58:20 +0000

    what it cors is gone

--- backend/laravel/config/cors.php
+++ backend/laravel/config/cors.php
@@ -3,10 +3,10 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     'allowed_origins' => [
-        'http://localhost:3000',                // local frontend dev
-        'http://pblcmlabs.duckdns.org:3000',  // if your frontend runs on this port
-        'http://pblcmlabs.duckdns.org:8000',  // backend URL (sometimes needed for API clients)
-        'https://pblcmlabs.duckdns.org',       // frontend URL in production, if https
+        // 'http://localhost:3000',                // local frontend dev
+        // 'http://pblcmlabs.duckdns.org:3000',  // if your frontend runs on this port
+        // 'http://pblcmlabs.duckdns.org:8000',  // backend URL (sometimes needed for API clients)
+        // 'https://pblcmlabs.duckdns.org',       // frontend URL in production, if https
     ], // use your VM's IP here
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-XSRF-TOKEN'],
