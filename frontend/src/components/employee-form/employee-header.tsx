327779de407396b85b41784ed98246ee790c1f76
ssh definitelynotjin <gkestiyanto@gmail.com>
2025-06-05 17:32:02 +0000

    lil thang

--- frontend/src/components/employee-form/employee-header.tsx
+++ frontend/src/components/employee-form/employee-header.tsx
@@ -3,7 +3,7 @@ const EmployeeSignInHeader = () => {
     <div className="flex flex-col items-start gap-2">
       <h1 className="text-3xl font-semibold">Employee Sign In</h1>
       <p className="text-muted-foreground text-sm">
-        Welcome back to HRIS CMLabs! Please sign in to continue managing your employee profile and accessing company resources.
+        Welcome back to HRIS CMLabs! Please sign in to continue.
       </p>
     </div>
   );
