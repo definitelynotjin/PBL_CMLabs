5465e38838a44c4d11f669eb2f2d2cd149df1b0e
mhankazis <mhankazis@gmail.com>
2025-06-06 20:10:26 +0700

    komatkamit

--- frontend/src/components/add-checkclock-admin/checkclock-form.tsx
+++ frontend/src/components/add-checkclock-admin/checkclock-form.tsx
@@ -5,7 +5,6 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-
 interface CheckclockFormProps {
   isClient: boolean;
 }
