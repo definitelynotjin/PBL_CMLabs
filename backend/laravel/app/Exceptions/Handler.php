fe29e23dc4c2e9f66472d30fe85149420cdb14dc
ssh definitelynotjin <gkestiyanto@gmail.com>
2025-06-11 02:25:15 +0000

    please

--- backend/laravel/app/Exceptions/Handler.php
+++ backend/laravel/app/Exceptions/Handler.php
@@ -8,15 +8,8 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    public function register()
-    {
-        //
-    }
-
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        return $request->expectsJson()
-            ? response()->json(['message' => 'Unauthenticated.'], 401)
-            : redirect()->guest(route('login'));
+        return response()->json(['message' => 'Unauthenticated.'], 401);
     }
 }
