02cd4cc87ec3a267abacbbd90d52997fdb75988b
ssh definitelynotjin <gkestiyanto@gmail.com>
2025-06-20 07:49:43 +0000

    wee

---
+++ backend/laravel/app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * Use '*' to trust all proxies.
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers used to detect proxies and scheme.
+     */
+    protected $headers = SymfonyRequest::HEADER_X_FORWARDED_FOR
+        | SymfonyRequest::HEADER_X_FORWARDED_HOST
+        | SymfonyRequest::HEADER_X_FORWARDED_PROTO;
+}
