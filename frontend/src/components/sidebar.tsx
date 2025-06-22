6367b24c5f4d2fefa5e17e22a24b6e53aed9d17a
ssh definitelynotjin <gkestiyanto@gmail.com>
2025-06-22 14:41:17 +0000

    try this

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -16,14 +16,14 @@ import {
   TooltipTrigger
 } from "@/components/ui/tooltip";
 
-const iconClass = "w-5 h-5 text-white hover:text-[#d1d1d1] transition-colors duration-200";
+const iconClass = "w-5 h-5 text-white hover:text-[#7CA5BF] transition-colors duration-200";
 
 
 const Sidebar: React.FC = () => (
   <TooltipProvider>
-    <aside className="w-16 h-screen flex flex-col justify-between items-center bg-[#595959] py-4">
+    <aside className="w-16 h-screen flex flex-col justify-between items-center bg-[#1E3A5F] py-4">
       <div className="flex flex-col items-center gap-6">
-        <Image src="/HRIS.png" alt="Logo" width={32} height={32} />
+        <Image src="/HRIS_White_Transparent.png" alt="Logo" width={32} height={32} />
 
         <Tooltip>
           <TooltipTrigger asChild>
